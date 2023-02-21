2d6433fd10c940e8868af33c25c903042083bb0b
3Anushka <anushkaj@sahaj.ai>
2023-02-21 19:39:53 +0530

    fetch username in url dynamically and get all errors

--- ESOP-Trading-Frontend/js/CreateOrder.js
+++ ESOP-Trading-Frontend/js/CreateOrder.js
@@ -2,12 +2,21 @@ const thisForm = document.getElementById('myForm');
 thisForm.addEventListener('submit', async function (e) {
     e.preventDefault();
     const formData = new FormData(thisForm).entries()
-    const response = await fetch('http://localhost:8080/user/amaan/order', {
+    console.log(username.value)
+    const response = await fetch(`http://localhost:8080/user/${username.value}/order`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(Object.fromEntries(formData))
-    });
+    }).then((response)=>response.json())
+    .then((json)=>{
+    console.log(json.errors)
+    if(json.errors){
+    document.getElementById("message").innerHTML = json.errors
 
-    const result = await response.json();
-    console.log(result)
+    }
+    })
+
+//    const result = await response.json();
+//    if(result.)
+//    console.log(result)
 });
