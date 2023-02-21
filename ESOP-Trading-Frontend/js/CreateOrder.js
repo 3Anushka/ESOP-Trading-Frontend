ed6bdced5573c12948417e08f5dbae7f44334b07
3Anushka <anushkaj@sahaj.ai>
2023-02-21 23:51:18 +0530

    Add getBody function

--- ESOP-Trading-Frontend/js/CreateOrder.js
+++ ESOP-Trading-Frontend/js/CreateOrder.js
@@ -1,12 +1,14 @@
 const thisForm = document.getElementById('myForm');
 thisForm.addEventListener('submit', async function (e) {
     e.preventDefault();
-    const formData = new FormData(thisForm).entries()
-    console.log(username.value)
-    const response = await fetch(`http://localhost:8080/user/${username.value}/order`, {
+        const queryString = new URLSearchParams(window.location.search);
+        const username = queryString.get('username')
+         console.log(username)
+    const response = await fetch(`http://localhost:8080/user/${username}/order`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(Object.fromEntries(formData))
+
+        body: getBody()
     }).then((response)=>response.json())
     .then((json)=>{
     console.log(json.errors)
@@ -14,9 +16,20 @@ thisForm.addEventListener('submit', async function (e) {
     document.getElementById("message").innerHTML = json.errors
 
     }
-    })
 
-//    const result = await response.json();
-//    if(result.)
-//    console.log(result)
-});
+})});
+
+function getBody() {
+
+    const orderType = document.getElementById('type').value
+    const price = document.getElementById('price').value
+
+    const quantity = document.getElementById('quantity').value
+
+    return JSON.stringify({
+        "type": orderType,
+        "quantity": quantity,
+        "price": price,
+
+    })
+}
